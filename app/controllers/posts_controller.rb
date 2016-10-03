6d6f1f88df8836a74f4928b163e04d850ef61343
giangson1604 <dgs160495@gmail.com>
2016-10-03 17:21:03 +0700

    them about

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,5 @@
 class PostsController < ApplicationController
-<<<<<<< HEAD
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
-=======
-<<<<<<< HEAD
-	before_action :find_post, only: [:show, :edit, :update, :destroy]
-=======
-	before_action :find_post, only: [:show, :edit, :update]
->>>>>>> origin/master
->>>>>>> origin/master
 
 	def index
 		@posts = Post.all.order('created_at DESC')
@@ -42,6 +34,10 @@ class PostsController < ApplicationController
 	def destroy
 		@post.destroy
 		redirect_to root_path
+	end
+
+	def about
+		
 	end
 	private
 	def find_post
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
   resources :posts
-<<<<<<< HEAD
   root 'posts#index'
-=======
-<<<<<<< HEAD
-  root 'posts#index'
-=======
-  root 'pots#index'
->>>>>>> origin/master
->>>>>>> origin/master
+  get '/about', to: 'posts#about'
 end
